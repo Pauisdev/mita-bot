1af4373628138b3ab7e0cb7532a7d8f5507c3036
fabrisdev <ezepuntocom@hotmail.com>
2026-03-31 23:50:46 -0300

    Comment out economy as its in WIP

--- events/messageCreate/economy.system.ts
+++ events/messageCreate/economy.system.ts
@@ -5,11 +5,11 @@ const COOLDOWN_TIME = 60_000;
 const lastUsed = new Map<string, number>();
 
 export default async (message: Message) => {
-	if (message.author.bot) return;
+	/*if (message.author.bot) return;
 	const userId = message.author.id;
 	const economy = Economy.of(userId);
 	const lastUsedAt = lastUsed.get(userId);
 	if (lastUsedAt && lastUsedAt + COOLDOWN_TIME > Date.now()) return;
 	lastUsed.set(userId, Date.now());
-	economy.addMoney();
+	economy.addMoney();*/
 };
